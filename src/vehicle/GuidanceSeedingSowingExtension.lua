294ad0c4fd1138c3046574d82f8f4d7c72c1cbc1
Stijn Wopereis <stijnwop@gmail.com>
2020-04-28 22:39:28 +0200

    Allow for playing empty sounds on multiple units and link to the attacher node for better sound quality.

--- src/vehicle/GuidanceSeedingSowingExtension.lua
+++ src/vehicle/GuidanceSeedingSowingExtension.lua
@@ -24,6 +24,7 @@ function GuidanceSeedingSowingExtension.registerEventListeners(vehicleType)
     SpecializationUtil.registerEventListener(vehicleType, "onLoad", GuidanceSeedingSowingExtension)
     SpecializationUtil.registerEventListener(vehicleType, "onDelete", GuidanceSeedingSowingExtension)
     SpecializationUtil.registerEventListener(vehicleType, "onUpdate", GuidanceSeedingSowingExtension)
+    SpecializationUtil.registerEventListener(vehicleType, "onDeactivate", GuidanceSeedingSowingExtension)
 end
 
 ---Called onLoad.
@@ -33,24 +34,30 @@ function GuidanceSeedingSowingExtension:onLoad(savegame)
 
     spec.fillUnitsToCheck = {}
     spec.fillUnitIndexForFrame = 1 -- current frame fillUnit to check
-    table.insert(spec.fillUnitsToCheck, Utils.getNoNil(self:getFirstValidFillUnitToFill(FillType.SEEDS, true), 1))
+    table.insert(spec.fillUnitsToCheck, { fillUnitIndex = Utils.getNoNil(self:getFirstValidFillUnitToFill(FillType.SEEDS, true), 1), didPlay = false })
 
     local fillUnitIndexFertilizer = self:getFirstValidFillUnitToFill(FillType.FERTILIZER, true)
     if fillUnitIndexFertilizer ~= nil then
-        table.insert(spec.fillUnitsToCheck, fillUnitIndexFertilizer)
+        table.insert(spec.fillUnitsToCheck, { fillUnitIndex = fillUnitIndexFertilizer, didPlay = false })
     end
 
     local fillUnitIndexLiquidFertilizer = self:getFirstValidFillUnitToFill(FillType.LIQUIDFERTILIZER, true)
     if fillUnitIndexLiquidFertilizer ~= nil then
-        table.insert(spec.fillUnitsToCheck, fillUnitIndexLiquidFertilizer)
+        table.insert(spec.fillUnitsToCheck, { fillUnitIndex = fillUnitIndexLiquidFertilizer, didPlay = false })
     end
 
     if self.isClient then
         --TODO: cleanup with better loading.
+        local linkNode = self.components[1].node
+        if self.getInputAttacherJoints ~= nil then
+            local inputAttacherJoint = self:getInputAttacherJoints()[1]
+            linkNode = inputAttacherJoint.node
+        end
+
         spec.samples = {}
         local sampleLowered = g_soundManager:loadSampleFromXML(self.xmlFile, "vehicle.guidanceSeeding.sounds", "lowered", self.baseDirectory, self.components, 1, AudioGroup.VEHICLE, self.i3dMappings, self)
         if sampleLowered == nil then
-            sampleLowered = g_soundManager:cloneSample(g_guidanceSeeding.samples.lowered, self.components[1].node, self)
+            sampleLowered = g_soundManager:cloneSample(g_guidanceSeeding.samples.lowered, linkNode, self)
         end
 
         spec.samples.lowered = sampleLowered
@@ -58,22 +65,23 @@ function GuidanceSeedingSowingExtension:onLoad(savegame)
 
         local sampleHighered = g_soundManager:loadSampleFromXML(self.xmlFile, "vehicle.guidanceSeeding.sounds", "highered", self.baseDirectory, self.components, 1, AudioGroup.VEHICLE, self.i3dMappings, self)
         if sampleHighered == nil then
-            sampleHighered = g_soundManager:cloneSample(g_guidanceSeeding.samples.highered, self.components[1].node, self)
+            sampleHighered = g_soundManager:cloneSample(g_guidanceSeeding.samples.highered, linkNode, self)
         end
 
         spec.samples.highered = sampleHighered
 
         local sampleEmpty = g_soundManager:loadSampleFromXML(self.xmlFile, "vehicle.guidanceSeeding.sounds", "empty", self.baseDirectory, self.components, 1, AudioGroup.VEHICLE, self.i3dMappings, self)
         if sampleEmpty == nil then
-            sampleEmpty = g_soundManager:cloneSample(g_guidanceSeeding.samples.empty, self.components[1].node, self)
+            sampleEmpty = g_soundManager:cloneSample(g_guidanceSeeding.samples.empty, linkNode, self)
         end
 
         spec.samples.empty = sampleEmpty
-        spec.playedEmpty = false
+        spec.activeFillUnitIndexEmptySound = nil
+        spec.activeFillUnitIndexAlmostEmptySound = nil
 
         local sampleTramline = g_soundManager:loadSampleFromXML(self.xmlFile, "vehicle.guidanceSeeding.sounds", "tramline", self.baseDirectory, self.components, 1, AudioGroup.VEHICLE, self.i3dMappings, self)
         if sampleTramline == nil then
-            sampleTramline = g_soundManager:cloneSample(g_guidanceSeeding.samples.tramline, self.components[1].node, self)
+            sampleTramline = g_soundManager:cloneSample(g_guidanceSeeding.samples.tramline, linkNode, self)
         end
 
         spec.samples.tramline = sampleTramline
@@ -88,6 +96,7 @@ function GuidanceSeedingSowingExtension:onDelete()
         g_soundManager:deleteSamples(spec.samples)
     end
 end
+
 function GuidanceSeedingSowingExtension:onUpdate(dt)
     local spec = self.spec_guidanceSeedingSowingExtension
 
@@ -131,32 +140,35 @@ function GuidanceSeedingSowingExtension:onUpdate(dt)
                 end
             end
 
-            local fillUnitIndex = spec.fillUnitsToCheck[math.min(spec.fillUnitIndexForFrame, #spec.fillUnitsToCheck)]
-            log(fillUnitIndex)
-            local fillPercentage = MathUtil.round(self:getFillUnitFillLevelPercentage(fillUnitIndex) * 100)
+            local desc = spec.fillUnitsToCheck[math.min(spec.fillUnitIndexForFrame, #spec.fillUnitsToCheck)]
+            local fillPercentage = MathUtil.round(self:getFillUnitFillLevelPercentage(desc.fillUnitIndex) * 100)
             local isAlmostEmpty = fillPercentage == 5 or fillPercentage == 1
+
+            local canPlayEmptySound = spec.activeFillUnitIndexEmptySound == desc.fillUnitIndex or spec.activeFillUnitIndexEmptySound == nil
             local isEmpty = fillPercentage == 0
 
             --Warning when empty.
-            if isEmpty then
+            if isEmpty and canPlayEmptySound then
                 if not g_soundManager:getIsSamplePlaying(spec.samples.empty) then
                     g_soundManager:playSample(spec.samples.empty)
+                    spec.activeFillUnitIndexEmptySound = desc.fillUnitIndex
                 end
-            elseif not isAlmostEmpty then
+            elseif not isAlmostEmpty and canPlayEmptySound then
                 if g_soundManager:getIsSamplePlaying(spec.samples.empty) then
                     g_soundManager:stopSample(spec.samples.empty)
+                    spec.activeFillUnitIndexEmptySound = nil
                 end
             end
 
             --Headsup warning when almost empty.
-            if not spec.playedEmpty then
+            if not desc.didPlay then
                 if isAlmostEmpty then
                     g_soundManager:playSample(spec.samples.empty, 1)
-                    spec.playedEmpty = true
+                    desc.didPlay = true
                 end
             else
                 if not isAlmostEmpty then
-                    spec.playedEmpty = false
+                    desc.didPlay = false
                 end
             end
 
@@ -169,3 +181,12 @@ function GuidanceSeedingSowingExtension:onUpdate(dt)
     end
 end
 
+
+function GuidanceSeedingSowingExtension:onDeactivate()
+    local spec = self.spec_guidanceSeedingSowingExtension
+    if self.isClient then
+        g_soundManager:stopSamples(spec.samples)
+        spec.playedTramline = false
+        spec.playedLowered = false
+    end
+end
