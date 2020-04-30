1abfea8e168d98538a4447ac0265ee2214635bbf
Stijn Wopereis <stijnwop@gmail.com>
2020-04-30 19:53:30 +0200

    Add overlay state support.

--- src/hud/elements/HUDButtonElement.lua
+++ src/hud/elements/HUDButtonElement.lua
@@ -21,7 +21,62 @@ function HUDButtonElement:new(overlay)
     instance.onClickCallback = nil
     instance.clickSoundName = GuiSoundPlayer.SOUND_SAMPLES.CLICK
 
+    self:setOverlayState(GuiOverlay.STATE_NORMAL)
+    self:loadOverlayColors(overlay)
+
     return instance
+end
+
+function HUDButtonElement:loadOverlayColors(overlay)
+    local color = { 1, 1, 1, 0.8 }
+    if color ~= nil then
+        overlay.color = color
+    end
+
+    local color = { 1, 1, 1, 1 }
+    if color ~= nil then
+        overlay.colorFocused = color
+    end
+
+    local color = { 1, 1, 1, 1 }
+    if color ~= nil then
+        overlay.colorPressed = color
+    end
+
+    local color = { 0.9910, 0.3865, 0.0100, 1 }
+    if color ~= nil then
+        overlay.colorSelected = color
+    end
+
+    local color = { 1, 1, 1, 1 }
+    if color ~= nil then
+        overlay.colorDisabled = color
+    end
+
+    local color = { 0.9910, 0.3865, 0.0100, 1 }
+    if color ~= nil then
+        overlay.colorHighlighted = color
+    end
+end
+
+function HUDButtonElement:setOverlayState(overlayState)
+    self.overlayState = overlayState
+end
+
+function HUDButtonElement:getOverlayState()
+    return self.overlayState
+end
+
+function HUDButtonElement:storeOverlayState()
+    self.previousOverlayState = self:getOverlayState()
+end
+
+function HUDButtonElement:restoreOverlayState()
+    if self.previousOverlayState then
+        --Restore overlay state from before
+        self:setOverlayState(self.previousOverlayState)
+        self.previousOverlayState = nil
+    end
 end
 
 ---Set the callback function
@@ -39,6 +94,16 @@ function HUDButtonElement:isActive()
     return self.onClickCallback ~= nil
 end
 
+---Called on draw
+function HUDButtonElement:draw()
+    local r, g, b, a = unpack(GuiOverlay.getOverlayColor(self.overlay, self.overlayState))
+    if a ~= 0 then
+        self.overlay:setColor(r, g, b, a)
+    end
+
+    HUDButtonElement:superClass().draw(self)
+end
+
 ---Called on mouse event.
 function HUDButtonElement:mouseEvent(posX, posY, isDown, isUp, button, eventUsed)
     eventUsed = HUDButtonElement:superClass().mouseEvent(self, posX, posY, isDown, isUp, button)
@@ -48,14 +113,19 @@ function HUDButtonElement:mouseEvent(posX, posY, isDown, isUp, button, eventUsed
         local cursorInElement = GuiUtils.checkOverlayOverlap(posX, posY, x, y, self:getWidth(), self:getHeight())
 
         if cursorInElement then
-            if not self.mouseEntered then
+            if not self.mouseEntered and not self.focusActive then
+                -- set highlight on mouse over without focus
+                FocusManager:setHighlight(self)
+
                 self.mouseEntered = true
             end
         else
             -- mouse event outside button
-            -- self:restoreOverlayState()
+            self:restoreOverlayState()
             self.mouseDown = false
             self.mouseEntered = false
+            --Reset highlight
+            FocusManager:unsetHighlight(self)
         end
 
         -- handle click/activate only if event has not been consumed, yet
@@ -67,15 +137,15 @@ function HUDButtonElement:mouseEvent(posX, posY, isDown, isUp, button, eventUsed
                 end
 
                 -- if needed, set state to PRESSED and store current overlay state for restoration
-                --if self.mouseDown and self:getOverlayState() ~= GuiOverlay.STATE_PRESSED then
-                --self:storeOverlayState()
-                --self:setOverlayState(GuiOverlay.STATE_PRESSED)
-                --end
+                if self.mouseDown and self:getOverlayState() ~= GuiOverlay.STATE_PRESSED then
+                    self:storeOverlayState()
+                    self:setOverlayState(GuiOverlay.STATE_PRESSED)
+                end
 
                 if isUp and button == Input.MOUSE_BUTTON_LEFT and self.mouseDown then
                     g_gui.soundPlayer:playSample(self.clickSoundName)
 
-                    --self:restoreOverlayState()
+                    self:restoreOverlayState()
                     self.mouseDown = false
                     self:raiseCallback("onClickCallback", self)
 
@@ -86,4 +156,20 @@ function HUDButtonElement:mouseEvent(posX, posY, isDown, isUp, button, eventUsed
     end
 
     return eventUsed
+end
+
+function HUDButtonElement:setSelected(isSelected)
+    if isSelected then
+        self:setOverlayState(GuiOverlay.STATE_SELECTED)
+    else
+        self:setOverlayState(GuiOverlay.STATE_NORMAL)
+    end
+end
+
+function HUDButtonElement:onHighlight()
+    self:raiseCallback("onHighlightCallback", self)
+end
+
+function HUDButtonElement:onHighlightRemove()
+    self:raiseCallback("onHighlightRemoveCallback", self)
 end
